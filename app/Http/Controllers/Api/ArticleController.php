9b862824ed5b9a9ecd4c0ef912c0b256a60d33c9
nicolus <nbailly@previsite.com>
2018-04-14 17:20:36 +0200

    Add Article Controller and Resource

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Article;
+use App\Http\Resources\ArticleResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return ArticleResource
+     */
+    public function index()
+    {
+        return ArticleResource::collection(Article::paginate());
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return $article;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+}
---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'date' => $this->created_at,
+            'feed' => new FeedResource($this->feed)
+        ];
+    }
+}
